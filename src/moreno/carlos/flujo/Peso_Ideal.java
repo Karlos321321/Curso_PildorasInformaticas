718fe41658704be4b98ffe0f2631f0354daa8deb
Karlos321321 <Karlos321321@gmail.com>
2025-11-11 22:24:56 +0100

    commit

--- src/moreno/carlos/flujo/Peso_Ideal.java
+++ src/moreno/carlos/flujo/Peso_Ideal.java
@@ -22,25 +22,24 @@ public class Peso_Ideal {
 					salida=false;
 				
 				} else {
-						do {
+					
+					do {
 							salida2 = true;	
 							genero = JOptionPane.showInputDialog("Hombre o Mujer h/m  ??");
 							
 							if(genero.equalsIgnoreCase("h")) {
-								
 								System.out.println("Eres un hombre y tu peso ideal es " + (altura-110));
-								
 								} else if(genero.equalsIgnoreCase("m")) {
-									
 									System.out.println("Eres una mujer y tu peso ideal es " + (altura-120));
-							
 									} else {
-												System.out.println(genero + " --> Genero incorrecto, intentalo de nuevo !!!!");
-												salida2 = false;
+										System.out.println(genero + " --> Genero incorrecto, intentalo de nuevo !!!!");
+										salida2 = false;
 										}
+						
 						} while(!salida2);
 						
 					}
+				
 			} catch (Exception e) {
 				// TODO: handle exception
 				System.out.println(e.fillInStackTrace() + " --> FORMATO DE DATOS INCORRECTO !!!!");
