09ff47b7a238241b18b4386292ed0f2ca3a3e4a6
Karlos321321 <Karlos321321@gmail.com>
2025-11-13 21:41:46 +0100

    commit

--- src/moreno/carlos/flujo/EjemploWhile1.java
+++ src/moreno/carlos/flujo/EjemploWhile1.java
@@ -1,23 +1,22 @@
 package moreno.carlos.flujo;
-
 import javax.swing.JOptionPane;
 
 public class EjemploWhile1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		String clave = "Juan";
-		String acceso = "";
+	
+		int numeroAleatorio = (int) (Math.random()*100);
+		int numeroIntroducido = 0;
+		int intentos = 0;
 		 
-		while (!clave.equalsIgnoreCase(acceso)) {
+		while (numeroIntroducido!=numeroAleatorio) {
 			
-			acceso = JOptionPane.showInputDialog("Introduce la clave de acceso ... ");
+			numeroIntroducido = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero entre 1 - 100 ... "));
+			intentos++;
 			
+			System.out.println(numeroIntroducido>numeroAleatorio ? "El numero secreto es menor !!!" : "El numero secreto es mayor !!!" );
 		}
 		
-		System.out.println("Bienvenido a la zona de usuarios !!!!");
-
+		System.out.println("Has necesitado " + intentos + " intentos, para hallar el numero secreto -->" + " '" + numeroAleatorio + "' ");
 	}
-
 }
---
+++ src/moreno/carlos/flujo/Ejemplo_Do_Whiie.java
@@ -0,0 +1,28 @@
+package moreno.carlos.flujo;
+import javax.swing.JOptionPane;
+
+public class Ejemplo_Do_Whiie {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Este programa cuenta los caracteres de la palabra o frase introducida\n"
+				+ "Para salir escribe 'Salir' ");
+		
+		String texto;
+		
+		/*while (!texto.equalsIgnoreCase("salir")) {
+			
+			texto = JOptionPane.showInputDialog("Introduce la frase a analizar ....");
+			if(!texto.equalsIgnoreCase("salir")) System.out.println("la frase introducida es --> " + texto + ", y tiene " + texto.length() + " caracteres !!!!");	
+		}*/
+		
+		do {	
+			
+			texto = JOptionPane.showInputDialog("Introduce la frase a analizar ....");
+			if(!texto.equalsIgnoreCase("salir")) System.out.println("la frase introducida es --> " + texto + ", y tiene " + texto.length() + " caracteres !!!!");
+			
+		} while (!texto.equalsIgnoreCase("salir"));
+		
+		System.out.println("\nHas introducido la palabra 'Salir' ");
+	}
+}
