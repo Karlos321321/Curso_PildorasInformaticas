d9842a7fcc9b5ceb6b6a9d8ec4a5a789cf6318bd
Karlos321321 <Karlos321321@gmail.com>
2025-11-16 18:23:39 +0100

    commit

---
+++ src/moreno/carlos/flujo/Email_Valido.java
@@ -0,0 +1,28 @@
+package moreno.carlos.flujo;
+import javax.swing.JOptionPane;
+
+public class Email_Valido {
+
+	public static void main(String[] args) {
+		
+		boolean salida = false;
+		String mail="";
+		
+		while (!salida) {
+			
+			int arroba=0;
+			int punto=0;
+			mail = JOptionPane.showInputDialog("Introduce una direccion de e-mail ...");
+			
+			for (int i = 0; i < mail.length(); i++) {
+				
+				if (mail.charAt(i)=='@') arroba++;
+				if (mail.charAt(i)=='.') punto++;
+			}
+			
+			if(arroba==1 && punto==1 && mail.length()>=4) salida=true;	
+		}
+		
+		System.out.println("Direccion de Email CORRECTA !!!!  ---  " + mail);
+	}
+}
